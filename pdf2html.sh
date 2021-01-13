b892fabdc55ba2b54d3c8555f32aa4494eee76d7
Brian Onang'o <surgbc@gmail.com>
2021-01-13 19:11:03 +0300

    SS20110101-01.pdf

--- pdf2html.sh
+++ pdf2html.sh
@@ -5,5 +5,9 @@ mkdir -p htmlFiles
 
 for file in $(ls "$CWD/files"); do 
     echo "$file"
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    echo "$filename"
     exit
 done
