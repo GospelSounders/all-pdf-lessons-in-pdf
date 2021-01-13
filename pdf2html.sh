43def7179acf0da68db2917fbdc96f39efcee035
Brian Onang'o <surgbc@gmail.com>
2021-01-13 19:12:31 +0300

    SS20111001-04.pdf

--- pdf2html.sh
+++ pdf2html.sh
@@ -9,5 +9,15 @@ for file in $(ls "$CWD/files"); do
     extension="${filename##*.}"
     filename="${filename%.*}"
     echo "$filename"
+    if test -f "$CWD/../htmlFiles/$filename.html"; then
+        echo "$file exists."
+    else
+        cp "$SOURCE_DIR/$file" "$CWD/files/$file"
+        echo "---->"
+        exit
+        git add .
+        git commit -m "$filename.html"
+        git push origin master
+    fi
     exit
 done
